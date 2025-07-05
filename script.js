9d610e9cfa79c5f9bf0db36fd0f0c195dc12a457
vitouh <donvitoreyes84@gmail.com>
2025-07-05 18:27:17 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let clickCount = 0;
+const content = document.getElementById("content");
+const scream = document.getElementById("scream");
+
+document.body.addEventListener("click", () => {
+  clickCount++;
+
+  if (clickCount === 1) {
+    document.body.classList.add("dark");
+    content.textContent = "why did you click this? don’t click me again.";
+  } else if (clickCount === 2) {
+    document.body.classList.add("jumpscare");
+    scream.play();
+  }
+});
